0f9f1cf9b617eae154bbe1b4e2b0b0cf1b9db4a7
АтмайкинСА <sam_samich_04@mail.ru>
2023-03-28 10:33:09 +0300

    1

---
+++ app/Http/Controllers/SpecialitiesController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\SpecialityResource;
+use App\Models\Specialities;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class SpecialitiesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json(SpecialityResource::collection(Specialities::all()));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+           'name' => 'required|unique:specialities'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 444);
+        }
+
+        $speciality = new Specialities();
+        $speciality->name = $request->name;
+
+        $speciality->save();
+
+        return response()->json([
+            'message' => 'success',
+            'data' => [
+                'speciality' => new SpecialityResource($speciality),
+            ]
+        ]);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Specialities $specialities)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Specialities $specialities)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Specialities $specialities)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Specialities $specialities)
+    {
+        //
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('phone')->unique();
             $table->date('birthday');
-            $table->tinyInteger('sex');
+            $table->boolean('sex');
+            $table->tinyInteger('role')->default(1);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
